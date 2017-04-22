ac172f58301a748f65b79f53816e1a415b8f6637
小島光平 <sesuna1997_winter@yahoo.co.jp>
2017-04-22 09:40:16 +0900

    Initial Commit

---
+++ Quiz/QuizViewController.swift
@@ -0,0 +1,99 @@
+//
+//  QuizViewController.swift
+//  Quiz
+//
+//  Created by litech on 2015/02/10.
+//  Copyright (c) 2015年 LifeisTech. All rights reserved.
+//
+
+import UIKit
+
+class QuizViewController: UIViewController {
+    //クイズを格納する配列
+    var quizArray = [[Any]]()
+    
+    //正解数
+    var correctAnswer:Int = 0
+    
+    //クイズを表示するTextView
+    @IBOutlet var quizTextView: UITextView!
+    
+    //選択肢のボタン
+    @IBOutlet var choiceButtons1: UIButton!
+    @IBOutlet var choiceButtons2: UIButton!
+    @IBOutlet var choiceButtons3: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        /* Swift3: [AnyObject]() -> [[Any]]() */
+        var tmpArray = [[Any]]()
+        
+        //------------------------ここから下にクイズを書く------------------------//
+        
+        
+        
+        tmpArray.append(["日本で一番長い川である信濃川が通ってない県は？","長野県","新潟県","富山県",3])
+        tmpArray.append(["日本で二番目に面積が大きな都道府県は？","福島県","岩手県","長野県",2])
+        tmpArray.append(["以下の中で中部地方に属さない府県は？","岐阜県","福井県","和歌山県",3])
+        tmpArray.append(["以下の中で、国立大学が一番多い府県は？","大阪府","茨城県","岩手県",2])
+        
+        
+        
+        //------------------------ここから上にクイズを書く------------------------//
+        
+        // 問題をシャッフルしてquizArrayに格納する
+        while (tmpArray.count > 0) {
+            let index = Int(arc4random_uniform(UInt32(tmpArray.count)))
+            quizArray.append(tmpArray[index])
+            tmpArray.remove(at: index)
+        }
+        choiceQuiz()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func choiceQuiz() {
+        quizTextView.text = quizArray[0][0] as! String
+        
+        //選択肢のボタンにそれぞれ選択肢のテキストをセット
+        choiceButtons1.setTitle(quizArray[0][1] as? String, for: .normal)
+        choiceButtons2.setTitle(quizArray[0][2] as? String, for: .normal)
+        choiceButtons3.setTitle(quizArray[0][3] as? String, for: .normal)
+    }
+    
+    @IBAction func choiceAnswer(sender: UIButton) {
+        if quizArray[0][4] as! Int == sender.tag {
+            //正解数を増やす
+            correctAnswer+=1
+        }
+        
+        quizArray.remove(at: 0)
+        //解いた問題数の合計が予め設定していた問題数に達したら結果画面へ
+        if quizArray.count == 0 {
+            performSegueToResult()
+        } else {
+            choiceQuiz()
+        }
+    }
+    
+    func performSegueToResult() {
+        performSegue(withIdentifier: "toResultView", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "toResultView") {
+            
+            let resultView = segue.destination as! ResultViewController
+            resultView.correctAnswer = self.correctAnswer
+        }
+    }
+    
+    
+    
+}
+
+
